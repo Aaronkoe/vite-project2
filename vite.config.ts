457c6cbab00a813797fdd13e57ff25b9433e3cc0
Aaron Koenigsberg <aaron.koenigsberg98@gmail.com>
2025-12-26 13:22:15 -0600

    Adds vite.config.ts and sets the directory to look for css and html

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // This tells Vite where your assets live relative to the domain
+  base: '/vite-project2/', 
+  
+  // If you are using React or Vue, you'd add the plugins here
+  plugins: [], 
+})
