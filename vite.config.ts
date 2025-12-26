7eae2b0fe74df667998eda1e37c31f6d6ef9cc92
Aaron Koenigsberg <aaron.koenigsberg98@gmail.com>
2025-12-26 13:34:18 -0600

    undoing relative path thing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // This tells Vite where your assets live relative to the domain
-  base: '/vite-project2/', 
-  
-  // If you are using React or Vue, you'd add the plugins here
   plugins: [], 
 })
